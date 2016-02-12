81bfcab130ad2c126460327ae675b1d37168631f
JohnJeffreyMahiban <johnjeffrey.mahiban@gmail.com>
2016-02-12 19:36:26 +0530

    Session1 Assignment 1

---
+++ S1Assignment1/src/SumOfTwoNumbers.java
@@ -0,0 +1,13 @@
+
+public class SumOfTwoNumbers {
+
+	public static void main(String[] args) {
+		// My First Assignment TO print sum of two numbers
+		
+			int a=10;
+			float  b=(float) 10.25;
+			System.out.println("The sum of two numbers is");
+			System.out.println(a+b);
+	}
+
+}
